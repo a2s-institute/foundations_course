6101bc60c1f80d552c20d2a698926541ef52bbbd
Tim Metzler <33530562+tmetzl@users.noreply.github.com>
2018-09-04 02:56:50 +0200

    Reorganized and cleaned up the installation script

--- content/ros/ROS Installation/ros_catkin_installer.sh
+++ content/ros/ROS Installation/ros_catkin_installer.sh
@@ -1,171 +1,259 @@
 #!/bin/bash
 
-# ROS installation and catkin initialization script
+#####################################################################################
+##                                                                                 ##
+##  ROS Installation script                                                        ##
+##                                                                                 ##
+##  This script acts as a little tool to install all necessary ROS packages        ##
+##  needed for the Autonomous Mobile Robots course at HBRS.                        ##
+##  It can also set up a catkin workspace and add ROS and catkin to your basrc.    ##
+##                                                                                 ##
+##  Note: This has been tested on a fresh Ubuntu 16.04 and with ROS Kinetic Kame   ##
+##  Tim Metzler 09/2018                                                            ##
+##                                                                                 ## 
+#####################################################################################
+
+# ROS packages
+# This is the ROS version that is used (i.e. indigo, kinetic, etc)
+ROS_VERSION=kinetic
+
+# These are the core ROS packages that are installed.
+# Each package in the array will be converted to ros-$ROS_VERSION-pkg_name (ros-kinetic-desktop)
+ROS_PKGS=(desktop stage smach-viewer map-server)
+
+# These are the python packages needed.
+# Each package in the array will be converted to python-pkg_name (e.g. python-rosinstall)
+PYTHON_PKGS=(catkin-tools rosinstall rosinstall-generator wstool pyside)
+
+# Additional packages to be installed
+OTHER_PKGS=(build-essential gnuplot)
+
+# Directory in which the script is executed
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function is_installed {
+  # Check if the package given as the first argument $1 is installed
+  # For this the dpkg-query output is searched for the keyphrase 'install ok installed'
+  PKG_INSTALLED=$(dpkg-query -W --showformat='${Status}' $1 2>/dev/null|grep "install ok installed")
+  if [ "$PKG_INSTALLED" == "install ok installed" ];
+  then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function install_pkg {
+  # Install the package given as the first argument $1
+  # Check if it is already present and check after installation for success
+  # If the installation is unsuccessful exit this script
+  echo ""
+  echo "Installing $1..."
+  # Check if package is already installed
+  if is_installed $1;
+  then
+    echo ""
+    echo "$1 is already installed"
+    return 0
+  else
+    # Install the package
+    sudo apt-get -y install $1
+    # Check if package was successfully installed
+    if is_installed $1;
+    then
+      echo ""
+      echo "Successfully installed package $1"
+      return 0
+    else
+      echo ""
+      echo "ERROR: Package $1 could not be installed!"
+      echo "Installation aborted!"
+      exit 1
+    fi
+  fi
+}
 
 function update_keys {
-	echo
-	echo "Updating keys and updating apt-get"
-	sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-	sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-	sudo apt-get update
+  echo
+  echo "Updating keys and updating apt-get"
+  sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+  sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+  sudo apt-get update
 }
 
 function ros_install {
-	echo
-	echo "Installing ROS Kinetic Kame"
-	sudo apt-get -y install ros-kinetic-desktop-full
-	sudo rosdep init
-	rosdep update
-	echo "Installing Smach Viewer"
-	sudo apt-get -y install ros-kinetic-smach-viewer
-	echo "Installing Map Server"
-	sudo apt-get -y install ros-kinetic-map-server
-	sudo apt-get -y install python-rosinstall python-rosinstall-generator python-wstool build-essential
-	sudo apt-get -y install python-pyside
-	sudo apt-get -y install gnuplot
-	source /opt/ros/kinetic/setup.bash
-	sudo chown -R $SUDO_USER:$SUDO_USER ~/.ros/
-	
+  # Installs ROS and additional packages needed for the AMR course
+  for pkg in ${ROS_PKGS[*]}; do
+    install_pkg ros-$ROS_VERSION-$pkg
+  done
+  sudo rosdep init
+  rosdep update
+  for pkg in ${PYTHON_PKGS[*]}; do
+    install_pkg python-$pkg
+  done
+  
+  for pkg in ${OTHER_PKGS[*]}; do
+    install_pkg $pkg
+  done
+
+  source /opt/ros/kinetic/setup.bash
+  #sudo chown -R $SUDO_USER:$SUDO_USER ~/.ros/
+  
 }
 
 function ros_bashrc {
-	echo
-	echo "Please choose if you want to add ROS to your bashrc."
-	echo "If you choose No you have to run the following in every terminal you want to use ROS in."
-	echo "source /opt/ros/kinetic/setup.bash"
-	PS3='Would you like to add ROS to your bashrc?: '
-	options=("Yes" "No")
-	select opt in "${options[@]}"
-	do
-	case $opt in
-		"Yes")
-			if grep -q 'source /opt/ros/kinetic/setup.bash' ~/.bashrc; then
-				echo "ROS Kinetic is already present in your bashrc."
-			else	
-				echo "Adding ROS to .bashrc. A backup file is created as .bashrc_before_ros"
-				cp ~/.bashrc ~/.bashrc_before_ros
-				echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
-				echo "Sourcing .bashrc"
-				source ~/.bashrc
-			fi
-			break
-			;;
-		"No")
-			break
-			;;
-		esac
-	done
+  echo
+  echo "Please choose if you want to add ROS to your bashrc."
+  echo "If you choose No you have to run the following in every terminal you want to use ROS in."
+  echo "source /opt/ros/kinetic/setup.bash"
+  PS3='Would you like to add ROS to your bashrc?: '
+  options=("Yes" "No")
+  select opt in "${options[@]}"
+  do
+  case $opt in
+    "Yes")
+      if grep -q 'source /opt/ros/kinetic/setup.bash' ~/.bashrc; then
+        echo "ROS Kinetic is already present in your bashrc."
+      else  
+        echo "Adding ROS to .bashrc. A backup file is created as .bashrc_before_ros"
+        cp ~/.bashrc ~/.bashrc_before_ros
+        echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+        echo "Sourcing .bashrc"
+        source ~/.bashrc
+      fi
+      break
+      ;;
+    "No")
+      break
+      ;;
+    esac
+  done
 }
 
 function ros_amr_bugfix {
-	echo
-	echo "Applying ROS bugfix"
-	cd $DIR
-	echo "Fixing stage.hh in opt/ros/kinetic/include/Stage-4.1/"
-	sudo cp bugfix/stage.hh /opt/ros/kinetic/include/Stage-4.1/stage.hh
-	echo "Fixing joint.hpp in opt/ros/kinetic/include/kdl/"
-	sudo cp bugfix/joint.hpp /opt/ros/kinetic/include/kdl/joint.hpp
-	echo "Applied bugfix."
+  # Fixes errors in the stage.hh and joint.hpp files
+  echo
+  echo "Applying ROS bugfix"
+  cd "$DIR"
+  echo "Fixing stage.hh in opt/ros/kinetic/include/Stage-4.1/"
+  # Check if stage.hh exists
+  if [ ! -f /opt/ros/kinetic/include/Stage-4.1/stage.hh ]
+  then
+    echo "ERROR: Could not find the original stage.hh file!" 
+    echo "Bugfix could not be applied!"
+    return
+  fi
+  sudo cp bugfix/stage.hh /opt/ros/kinetic/include/Stage-4.1/stage.hh
+
+  echo "Fixing joint.hpp in opt/ros/kinetic/include/kdl/"
+  # Check if joint.hpp exists
+  if [ ! -f /opt/ros/kinetic/include/kdl/joint.hpp ]
+  then
+    echo "ERROR: Could not find the original joint.hpp file!" 
+    echo "Bugfix could not be applied!"
+    return
+  fi
+  sudo cp bugfix/joint.hpp /opt/ros/kinetic/include/kdl/joint.hpp
+  echo "Applied bugfix."
 }
 
 function create_catkin_workspace {
-	source /opt/ros/kinetic/setup.bash
-	echo
-	echo "Initializing catkin workspace"
-	cd $HOME
-	echo "Creating directories"
-	mkdir catkin_ws
-	cd catkin_ws
-	mkdir src
-	cd src
-	catkin_init_workspace
-	cd ..
-	catkin_make
-	echo "Sourcing the workspace"
-	source devel/setup.bash
-	sudo chown -R $SUDO_USER:$SUDO_USER ~/catkin_ws/
+  source /opt/ros/kinetic/setup.bash
+  echo
+  echo "Initializing catkin workspace"
+  cd $HOME
+  echo "Creating directories"
+  mkdir catkin_ws
+  cd catkin_ws
+  mkdir src
+  cd src
+  cd ..
+  catkin init
+  catkin build
+  echo "Sourcing the workspace"
+  source devel/setup.bash
 }
 
 function catkin_bashrc {
-	echo
-	echo "Please choose if you want to add Catkin to your bashrc."
-	echo "If you choose No you have to run the following in every terminal you want to use catkin commands in."
-	echo "source ~/catkin_ws/devel/setup.bash"
-	PS3='Would you like to add Catkin to your bashrc?: '
-	options=("Yes" "No")
-	select opt in "${options[@]}"
-	do
-	case $opt in
-		"Yes")
-			if grep -q 'catkin_ws/devel/setup.bash' ~/.bashrc; then
-				echo "A catkin workspace is already present in your bashrc."
-			else
-				echo "Adding catkin to .bashrc. A backup file is created as .bashrc_before_catkin"
-				cp ~/.bashrc ~/.bashrc_before_catkin
-				echo "Adding catkin workspace to bashrc"
-				echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
-				echo "Sourcing .bashrc"
-				source ~/.bashrc
-			fi
-			break
-			;;
-		"No")
-			break
-			;;
-		esac
-	done
+  echo
+  echo "Please choose if you want to add Catkin to your bashrc."
+  echo "If you choose No you have to run the following in every terminal you want to use catkin commands in."
+  echo "source ~/catkin_ws/devel/setup.bash"
+  PS3='Would you like to add Catkin to your bashrc?: '
+  options=("Yes" "No")
+  select opt in "${options[@]}"
+  do
+  case $opt in
+    "Yes")
+      if grep -q 'catkin_ws/devel/setup.bash' ~/.bashrc; then
+        echo "A catkin workspace is already present in your bashrc."
+      else
+        echo "Adding catkin to .bashrc. A backup file is created as .bashrc_before_catkin"
+        cp ~/.bashrc ~/.bashrc_before_catkin
+        echo "Adding catkin workspace to bashrc"
+        echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+        echo "Sourcing .bashrc"
+        source ~/.bashrc
+      fi
+      break
+      ;;
+    "No")
+      break
+      ;;
+    esac
+  done
 }
 
 function main_menu {
-	echo 
-	echo "Please select an option:"
-	echo
-	PS3='What would you like to do?: '
-	options=("Install ROS" "Apply ROS bugfix" "Create catkin workspace" "Install everything" "Quit")
-	select opt in "${options[@]}"
-	do
-	case $opt in
-		"Install ROS")
-			update_keys
-			ros_install
-			ros_bashrc
-			main_menu
-			break
-			;;
-		"Apply ROS bugfix")
-			ros_amr_bugfix
-			main_menu
-			break
-			;;
-		"Create catkin workspace")
-			create_catkin_workspace
-			catkin_bashrc
-			main_menu
-			break
-			;;
-		"Install everything")
-			update_keys
-			ros_install
-			ros_bashrc
-			ros_amr_bugfix
-			create_catkin_workspace
-			catkin_bashrc
-			echo
-			echo "Installed everything"
-			echo "Goodbye !"
-			exit 0
-			;;
-		"Quit")
-			echo "Goodbye !"
-			exit 0
-			;;	
-		esac
-	done
+  echo 
+  echo "Please select an option:"
+  echo
+  PS3='What would you like to do?: '
+  options=("Install everything" "Install ROS" "Apply ROS bugfix" "Create catkin workspace" "Quit")
+  select opt in "${options[@]}"
+  do
+  case $opt in
+    "Install everything")
+      update_keys
+      ros_install
+      ros_bashrc
+      ros_amr_bugfix
+      create_catkin_workspace
+      catkin_bashrc
+      echo
+      echo "Installed everything"
+      echo "Goodbye !"
+      exit 0
+      ;;
+    "Install ROS")
+      update_keys
+      ros_install
+      ros_bashrc
+      main_menu
+      break
+      ;;
+    "Apply ROS bugfix")
+      ros_amr_bugfix
+      main_menu
+      break
+      ;;
+    "Create catkin workspace")
+      create_catkin_workspace
+      catkin_bashrc
+      main_menu
+      break
+      ;;
+    "Quit")
+      echo "Goodbye !"
+      exit 0
+      ;;  
+    esac
+  done
 }
 
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     echo "Please execute this script as sudo!"
     exit
 fi
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 main_menu
