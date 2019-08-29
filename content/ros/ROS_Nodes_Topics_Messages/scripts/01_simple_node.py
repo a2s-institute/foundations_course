f583e5ceb51320ce4dd73dfb7bb08050ea2d4949
hasauino <oh91@windowslive.com>
2019-08-29 11:34:09 +0200

    update day2 ROS session slides

--- content/ros/ROS_Nodes_Topics_Messages/scripts/01_simple_node.py
+++ content/ros/ROS_Nodes_Topics_Messages/scripts/01_simple_node.py
@@ -3,9 +3,13 @@
 import rospy
 
 
-rospy.init_node('print_text')  
-rate = rospy.Rate(1) # an isntance of Rate class with 1 Hz as argument
+rospy.init_node('print_text')
 
-while not rospy.is_shutdown():  # checks when the node receives a shutdown request (ex. Ctrl+C)
+# an isntance of Rate class with 1 Hz as argument
+rate = rospy.Rate(1)
+
+# checks when the node receives a shutdown request (ex. Ctrl+C)
+while not rospy.is_shutdown():
     print "Hello world!"
-    rate.sleep()  # makes sure the time between each call is at the specified rate
+    # make sure the time between each call is at the specified rate
+    rate.sleep()
