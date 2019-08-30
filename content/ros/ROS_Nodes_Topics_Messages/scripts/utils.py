a99cd610a8a74d15bd7189b48aed91a5b917156a
hasauino <oh91@windowslive.com>
2019-08-30 21:48:40 +0200

    update ros day2 session material

--- content/ros/ROS_Nodes_Topics_Messages/scripts/utils.py
+++ content/ros/ROS_Nodes_Topics_Messages/scripts/utils.py
@@ -14,9 +14,16 @@ import numpy as np
 class Turtle:
     # constructor
     def __init__(self, turtle_number=1):
+        """Turtle class provides interface to control the turtle and get 
+           feedback
+        
+        Keyword Arguments:
+            turtle_number {int} -- turtle's number (default: {1})
+        """
         # define a private variable of type Publisher (an instance of Publisher)
         self.__pub = rospy.Publisher('/turtle'+str(turtle_number)+'/cmd_vel',
                                      Twist, queue_size=10)
+        self.__vel_msg = Twist()
         # define subscribers for pose and color topics, and assign callbacks
         rospy.Subscriber('/turtle'+str(turtle_number)+'/pose', Pose,
                          callback=self.__pose_cb)
@@ -51,12 +58,41 @@ class Turtle:
 
     # a public method to be used by the user to get pose
     def get_pose(self):
+        """get the current pose (position and orientation) of the turtle
+        
+        Returns:
+            numpy array -- 3x1 array: x (m),y (m),theta (rad)
+        """
         return self.__pose
 
     # a public method to be used by the user to get velocity
     def get_velocity(self):
+        """get the current velocity of the turtle
+        
+        Returns:
+            numpy array -- 3x1 array: linear velocity in x (m/s), linear 
+                           velocity in y (always zero), angular velocity around
+                           the z (rad/s)
+        """
         return self.__velocity
 
     # a public method to be used by the user to get color reading
     def sense_color(self):
+        """returns color reading of the turtle's color sensor
+        
+        Returns:
+            list -- [r,g,b] representing RGB values
+        """
         return self.__color
+
+    # a public method to be used by the user to set the velocity
+    def set_velocity(self, v, w):
+        """sends a velocity command to the turtle
+        
+        Arguments:
+            v {float} -- linear velocity (m/s)
+            w {[type]} -- angular velocity (rad/s)
+        """
+        self.__vel_msg.linear.x = v
+        self.__vel_msg.angular.z = w
+        self.__pub.publish(self.__vel_msg)
