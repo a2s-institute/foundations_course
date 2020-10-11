61d3858d7bcdf99138f1e014423c98c0a0b4d836
malwaru <malikawarunamal@gmail.com>
2020-10-11 15:44:10 +0530

    Backup v2

--- exercises/cpp/guessbot.cpp
+++ exercises/cpp/guessbot.cpp
@@ -63,7 +63,7 @@ return 0;
 void mode1()
 {
     cout<<"Hello you entered game mode 1"<<endl;
-    cout<<"The bot will think of a number between 1 and 100 , your have to guess that number"<<endl;
+    cout<<"The bot will think of a number between 1 and 100 , your have to guess that number \n"<<endl;
     int random_number= rand()%100;
     int guess;
     int count=0;
@@ -84,18 +84,20 @@ void mode1()
         }
         
         count++;
+        cout<<"Enter you guess"<<endl;
+        cin>>guess;
         
 
     }
     
     if(guess==random_number)
     {
-        cout<<"Congradulations you guessed in "<<count<<" tries"<<endl;
+        cout<<"\n Congradulations you guessed in "<<count<<" tries"<<endl;
     }
     
     else
     {
-        cout<<"Sorry you could'nt guess in 20 tries"<<endl;
+        cout<<"\n Sorry you could'nt guess in 20 tries"<<endl;
     }
     
 
@@ -110,7 +112,9 @@ void mode2()
     cout<<"You will have to think of a number between 1 and 100 , dumb bot will have to guess that number"<<endl;
     cout<<"If the bot guessed right enter 'r' "<<endl;
     cout<<"If the bot guessed wrong and if the actual number is higher enter 'h' "<<endl;
-    cout<<"If the bot guessed wrong and if the actual number is lower enter 'l' "<<endl;
+    cout<<"If the bot guessed wrong and if the actual number is lower enter 'l' \n "<<endl;
+
+    cout<<"Think of a number, the game starts now \n "<<endl;
 
     int guess= rand()%100;;
     
